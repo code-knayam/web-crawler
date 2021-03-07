9c33459733e8fab65f0e3309dd6a7d22d60142d7
Mayank Jain <mayankjain1394@gmail.com>
2021-03-07 19:57:39 +0530

    added output file

--- index.js
+++ index.js
@@ -5,12 +5,29 @@ const sleep = util.promisify(setTimeout);
 const cheerio = require("cheerio");
 const { URL } = require("url");
 const { parse } = require("path");
+const fs = require("fs");
+
+
+fs.writeFile("./visited_links.txt", ``, function (err) {
+	if (err) {
+		return console.error(err);
+	}
+	console.info("Visited link file was reset!");
+});
+
+fs.writeFile("./links_queue.txt", ``, function (err) {
+	if (err) {
+		return console.error(err);
+	}
+	console.info("Link queue file was reset!");
+});
 
 let seenLinks = new Set();
 
 let linksQueue = [];
-let maxCrawlingDepth = 5;
-
+const maxCrawlingDepth = 5;
+const maxQueueSize = 2000;
+const seenLinkLimit = 100;
 let mainDomain = null;
 let mainParsedUrl = null;
 
@@ -25,49 +42,65 @@ class CreateLink {
 
 // main entry function
 
-startCrawl("https://www.youtube.com/", 1);
+startCrawl("https://github.com/code-knayam/me", 1);
 
 async function startCrawl(url, maxDepth = 5) {
 	try {
 		mainParsedUrl = new URL(url);
 	} catch (e) {
-		console.log("Invalid Url", e);
+		console.error("Invalid Url", e);
 		return;
 	}
 
 	mainDomain = mainParsedUrl.hostname;
 
 	startLink = new CreateLink(url, 0, null);
-
-    await findLinks(startLink);
+	console.info("Started Scrapping");
+	await findLinks(startLink);
 }
 
 async function crawl(linkObj) {
 	if (
-		seenLinks.size > 50 ||
+		seenLinks.size > seenLinkLimit ||
 		linksQueue.length === 0 ||
-		linksQueue.length > 1000
+		linksQueue.length > maxQueueSize
 	) {
-		console.log(seenLinks);
+		console.info("Finished Scrapping");
+
+		console.info("Started dumpping output to file");
+		
+		addSeenLinksToFile();
+		addLinksQueueToFile();
+		
+		console.info("Finished dump");
 		return;
 	}
+
 	await findLinks(linkObj);
 }
 
 async function findLinks(linkObj) {
 	const urlToLoad = linkObj.url;
+	
 	try {
 		response = await rp(urlToLoad);
 
+		console.debug(linksQueue.length, seenLinks.size)
+
 		if (response.statusCode !== 200) {
+			console.info('Status error')
+			crawl(getNextLink());
 			return;
 		}
 
 		if (linkObj.depth > maxCrawlingDepth) {
+			console.info('Max depth')
+			crawl(getNextLink());
 			return;
 		}
 
 		let $ = cheerio.load(response.body);
+
 		let links = $("body")
 			.find("a")
 			.filter(function (i, el) {
@@ -76,7 +109,6 @@ async function findLinks(linkObj) {
 			.map(function (i, x) {
 				return $(this).attr("href");
 			});
-
 		if (links.length > 0) {
 			links = links
 				.map((_, link) => {
@@ -89,7 +121,7 @@ async function findLinks(linkObj) {
 				})
 				.filter(Boolean);
 		} else {
-			console.log("No more links found for " + urlToLoad);
+			console.info("No more links found for " + urlToLoad);
 		}
 
 		addToLinksQueue(links);
@@ -97,7 +129,7 @@ async function findLinks(linkObj) {
 
 		crawl(getNextLink());
 	} catch (e) {
-		console.log(e);
+		console.error(e);
 		return;
 	}
 }
@@ -132,4 +164,28 @@ function addToVisitedLink(link) {
 
 function getNextLink() {
 	return linksQueue.shift();
+}
+
+function isWatchVideo(parsedLink) {
+	return parsedLink.searchParams['v'] && parsedLink.pathname === 'watch';
+}
+
+function addSeenLinksToFile() {
+	for (let link of seenLinks) {
+		fs.appendFile("./visited_links.txt", `${link} \n`, function (err) {
+			if (err) {
+				return console.error(err);
+			}
+		});
+	}
+}
+
+function addLinksQueueToFile() {
+	for (let link of linksQueue) {
+		fs.appendFile("./links_queue.txt", `${link.url} \n`, function (err) {
+			if (err) {
+				return console.error(err);
+			}
+		});
+	}
 }
